6eba493700438401fdac9131653d1be3925af1a4
quangthongvo <quangthongvo02@gmail.com>
2024-06-18 09:25:32 +0700

    Create assignment-06

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -2,8 +2,8 @@ import util.ScannerUtil;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Nhap vao so nguyen ");
-        int n = ScannerUtil.intputPositiveInt();
-        System.out.println("n = " + n);
+        System.out.println("Nhap vao full name  ");
+        String fullName = ScannerUtil.inputFullName();
+        System.out.println("fullName = " + fullName);
     }
 }
--- src/main/java/util/ScannerUtil.java
+++ src/main/java/util/ScannerUtil.java
@@ -67,30 +67,53 @@ public class ScannerUtil {
                     System.out.println("Yeu cau nhap lai @");
                     System.out.println(" Nhap lai : ");
                 }
-            }catch (NumberFormatException exception) {
+            } catch (NumberFormatException exception) {
                 System.out.println("Yeu cau nhap vao so nguyen.");
                 System.out.println(" Nhap lai : ");
             }
         }
     }
-    public static String inputPassword(){
+
+    public static String inputPassword() {
         while (true) {
             String input = scanner.nextLine();
             int length = input.length();
-            if (length < 6 || length > 12){
+            if (length < 6 || length > 12) {
                 System.out.println("Yeu cau nhap tu 6 den 12 ki tu");
                 System.out.println(" Nhap lai : ");
             } else {
-                for (int i = 0; i < length; i++){
+                for (int i = 0; i < length; i++) {
                     char c = input.charAt(i);
-                    if (Character.isUpperCase(c)){
+                    if (Character.isUpperCase(c)) {
                         return input;
                     }
                 }
-                System.out.println("Yeu cau nhap it nhat1 ki tu viet hoa");
+                System.out.println("Yeu cau nhap it nhat 1 ki tu viet hoa");
                 System.out.println(" Nhap lai : ");
             }
         }
     }
 
+    public static String inputFullName() {
+        while (true) {
+            String input = scanner.nextLine();
+            if (hasAllAlphabetic(input)) {
+                return input;
+            } else {
+                System.out.println("yeu cau full name chi chua chu cai ");
+                System.out.println("Nhap lai : ");
+            }
+        }
+    }
+    private static boolean hasAllAlphabetic(String s){
+        int length = s.length();
+        for(int i = 0; i < length ; i++){
+            char c = s.charAt(i);
+            if(Character.isWhitespace(c))continue;
+            if (!Character.isAlphabetic(c)){
+             return false;
+            }
+        }
+        return true;
+    }
 }
