1012519239abba8c24d8aac0fce7d8a41b51ef24
quangthongvo <quangthongvo02@gmail.com>
2024-06-22 19:08:16 +0700

    Create assignment-07

---
+++ src/main/java/exercise02/Student.java
@@ -0,0 +1,11 @@
+package exercise02;
+
+public class Student {
+    private final int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
